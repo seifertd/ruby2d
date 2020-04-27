853d62dcc279e77df82991a6780ec601d2df73c2
Douglas A. Seifert <doug@dseifert.net>
2020-04-26 20:18:25 -0700

    redo integration in world space

--- nbody.rb
+++ nbody.rb
@@ -33,7 +33,7 @@ class Body
     nr = (biggest.radius ** 3 + smallest.radius ** 3 ) ** (1.0 / 3.0)
     vnx = (biggest.mass * biggest.vel.x + smallest.mass * smallest.vel.x) / ( biggest.mass + smallest.mass )
     vny = (biggest.mass * biggest.vel.y + smallest.mass * smallest.vel.y) / ( biggest.mass + smallest.mass )
-    puts "COLLISION! r=#{nr.round(3)} m=#{"%.3e" % (biggest.mass + smallest.mass)} vx=#{vnx.round(3)} vy=#{vny.round(3)}"
+    puts "COLLISION! r=#{"%.5e" % nr} m=#{"%.5e" % (biggest.mass + smallest.mass)} vx=#{"%.5e" % vnx} vy=#{"%.5e" % vny}"
     Body.new(biggest.pos.x, biggest.pos.y, nr, biggest.mass + smallest.mass, vnx, vny)
   end
 end
@@ -49,17 +49,18 @@ class NBody < Gosu::Window
     @width = width
     @height = height
     @running = true
-    @pin_barycenter = true
+    @pin_barycenter = false
+    @offset = Vector.new(@width/2, @height/2)
     @display = true
     @scale = 0.1
     @elapsed = 0
     # seconds per tick
     @spt = 60
     @bodies = [
-      Body.new(width / 2, height / 2, 30, 5e28, 0, 0)
+      Body.new(0, 0, 30 * MPP, 5e28, 0, 0)
     ]
     center = @bodies.first
-    random_gen(30, 1.0, true)
+    random_gen(50, 1.0, false)
     #random_with_moons(5,3)
     super(width, height)
     @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
@@ -71,33 +72,33 @@ class NBody < Gosu::Window
   end
 
   def random_with_moons(n, m)
-    @scale = 0.2
+    @scale = 0.3
     center = @bodies.first
     n.times do |i|
-      x = -@width * 3 +  rand(@width * 6)
-      y = -@height * 3 + rand(@height * 6)
-      r = Vector.new(@width/2.0 - x, @height/2.0 - y)
-      circular_orbit_vel =  Math.sqrt(G * center.mass / (r.magnitude * MPP)) / MPP
+      x = (-@width * 4 + rand(@width) * 8) * MPP
+      y = (-@height * 4 + rand(@height)* 8) * MPP
+      r = Vector.new(center.pos.x - x, center.pos.y - y)
+      circular_orbit_vel =  Math.sqrt(G * center.mass / r.magnitude)
       vel = r.unit.normal * circular_orbit_vel
       mass = rand() * 1e26
-      radius = 8 + rand(8)
+      radius = (8 + rand(8)) * MPP
       @bodies <<  Body.new(x, y, radius, mass, vel.x, vel.y)
       body = @bodies.last
-      puts "BODY: #{i}: m:#{"%.3e" % body.mass} vel:#{body.vel.x.round(3)},#{body.vel.y.round(3)} pos:#{body.pos.x},#{body.pos.y}"
+      puts "BODY: #{i}: m:#{"%.5e" % body.mass} vel:#{"%.5e" % body.vel.x},#{"%.5e" % body.vel.y} pos:#{"%.5e" % body.pos.x},#{"%.5e" % body.pos.y}"
 
       m.times do |j|
         #moon
-        d = radius + 10 + rand(50)
+        d = radius + (10 + rand(40)) * MPP
         # moon unit normal
-        moon_orbit_vel = Math.sqrt(G * mass / (d*MPP)) / MPP
+        moon_orbit_vel = Math.sqrt(G * mass / d)
         sign = j == 1 ? 1 : -1
         moon_un = Vector.new(0, sign)
         moon_vel = vel + (moon_un * moon_orbit_vel)
         moon_mass = 1e7 * rand()
-        moon_radius = 1 + rand(4)
+        moon_radius = (1 + rand(4)) * MPP
         @bodies <<  Body.new(x - sign * d, y, moon_radius, moon_mass, moon_vel.x, moon_vel.y)
         body = @bodies.last
-        puts "MOON: #{i}: m:#{"%.3e" % body.mass} vel:#{body.vel.x.round(3)},#{body.vel.y.round(3)} pos:#{body.pos.x},#{body.pos.y}"
+        puts "MOON: #{i}: m:#{"%.5e" % body.mass} vel:#{"%.5e" % body.vel.x},#{"%.5e" % body.vel.y} pos:#{"%.5e" % body.pos.x},#{"%.5e" % body.pos.y}"
       end
     end
   end
@@ -107,23 +108,23 @@ class NBody < Gosu::Window
     center = @bodies.first
     n.times do |i|
       if dense
-        x = rand(@width)
-        y = rand(@height)
+        x = (-@width / 4 + rand(@width) / 2) * MPP
+        y = (-@height / 4 + rand(@height) / 2) * MPP
       else
-        x = -@width  + rand(@width * 3)
-        y = -@height + rand(@height * 3)
+        x = (-@width / 2 + rand(@width)) * MPP
+        y = (-@height / 2 + rand(@height)) * MPP
       end
-      r = Vector.new(@width/2.0 - x, @height/2.0 - y)
-      circular_orbit_vel =  Math.sqrt(G * center.mass / (r.magnitude * MPP)) / MPP
+      r = Vector.new(center.pos.x - x, center.pos.y - y)
+      circular_orbit_vel =  Math.sqrt(G * center.mass / r.magnitude)
       vel = r.unit.normal * circular_orbit_vel
       # Perturb just a tad
       vel.x = vel.x * (1.0 - (pf/2.0) + rand() * pf)
       vel.y = vel.y * (1.0 - (pf/2.0) + rand() * pf)
       base_mass = i < (n/2) ? 1e22 : 1e7
       base_radius = i < (n/2) ? 10 : 4
-      @bodies <<  Body.new(x, y, 1 + rand(base_radius), base_mass * rand(), vel.x, vel.y)
+      @bodies <<  Body.new(x, y, (1 + rand(base_radius)) * MPP, base_mass * rand(), vel.x, vel.y)
       body = @bodies.last
-      puts "BODY: #{i}: m:#{"%.3e" % body.mass} vel:#{body.vel.x.round(3)},#{body.vel.y.round(3)} pos:#{body.pos.x},#{body.pos.y}"
+      puts "BODY: #{i}: m:#{"%.5e" % body.mass} vel:#{"%.5e" % body.vel.x},#{"%.5e" % body.vel.y} pos:#{"%.5e" % body.pos.x},#{"%.5e" % body.pos.y}"
     end
   end
 
@@ -143,7 +144,7 @@ class NBody < Gosu::Window
           d = Math.sqrt((body.pos.x - body2.pos.x)**2 + (body.pos.y - body2.pos.y)**2)
           acc_vect = Vector.new((body2.pos.x - body.pos.x) / d, (body2.pos.y - body.pos.y) / d)
           acc_vect = Vector.new((body2.pos.x - body.pos.x) / d, (body2.pos.y - body.pos.y) / d)
-          body.acc.add! (acc_vect * (G * body2.mass / (d*d*MPP*MPP) / MPP))
+          body.acc.add! (acc_vect * (G * body2.mass / (d*d)))
         end
       end
       @bodies.each do |body|
@@ -164,20 +165,6 @@ class NBody < Gosu::Window
           @bodies.delete(body2)
           @bodies << body1.collide_with(body2)
         end
-        if body.pos.x > bc.x + 10000 || body.pos.x < bc.x - 10000 ||
-           body.pos.y > bc.y + 10000 || body.pos.x < bc.y - 10000
-          @bodies.delete(body)
-          puts "REMOVE: vel: #{body.vel.x.round(3)},#{body.vel.y.round(3)} pos: #{body.pos.x.round(3)},#{body.pos.y.round(3)}"
-        end
-      end
-    end
-    if @pin_barycenter
-      pos = barycenter
-      dx = @width / 2.0 / @scale - pos.x
-      dy = @height / 2.0 / @scale - pos.y
-      @bodies.each do |body|
-        body.pos.x += dx
-        body.pos.y += dy
       end
     end
     if @bodies.size <= 1
@@ -192,38 +179,12 @@ class NBody < Gosu::Window
   def button_down(id)
     if id == Gosu::KbSpace
       @running = !@running
-    elsif id == Gosu::KbW
-      @bodies.each do |body|
-        body.pos.y -= 10
-      end
-    elsif id == Gosu::KbS
-      @bodies.each do |body|
-        body.pos.y += 10
-      end
-    elsif id == Gosu::KbA
-      @bodies.each do |body|
-        body.pos.x -= 10
-      end
-    elsif id == Gosu::KbD
-      @bodies.each do |body|
-        body.pos.x += 10
-      end
     elsif id == Gosu::KbC
+      @offset = Vector.new @width / 2, @height / 2
+    elsif id == Gosu::KbB
       @pin_barycenter = !@pin_barycenter
     elsif id == Gosu::KbI
       @display = !@display
-    elsif id == Gosu::KbT
-      if @scale > 0.2
-        @scale -= 0.1
-      end
-    elsif id == Gosu::KbG
-      @scale += 0.1
-    elsif id == Gosu::KbO
-      @spt += 1
-    elsif id == Gosu::KbL
-      if @spt > 2
-        @spt -= 1
-      end
     end
 
   end
@@ -243,19 +204,57 @@ class NBody < Gosu::Window
         weighted_pos += (body.pos * body.mass)
       end
       weighted_pos / total_mass
-    else
-      Vector.new(@width / 2, @height / 2)
     end
   end
 
+  def world_to_screen(world)
+    Vector.new(
+      @offset.x + world.x * @scale / MPP,
+      @offset.y + world.y * @scale / MPP
+    )
+  end
+
   def draw
+    if button_down? Gosu::KbT
+      @scale *= 0.9
+    elsif button_down? Gosu::KbG
+      @scale *= 1.1
+    elsif button_down? Gosu::KbW
+      @offset.y -= 10
+    elsif button_down? Gosu::KbS
+      @offset.y += 10
+    elsif button_down? Gosu::KbA
+      @offset.x -= 10
+    elsif button_down? Gosu::KbD
+      @offset.x += 10
+    elsif button_down? Gosu::KbO
+      @spt += 1
+    elsif button_down? Gosu::KbL
+      if @spt > 2
+        @spt -= 1
+      end
+    end
+    bc = barycenter
+    bc_sc = world_to_screen(barycenter)
+    if @pin_barycenter
+      @offset.x = @offset.y = 0
+      @offset = Vector.new(@width/2,@height/2) + world_to_screen(bc - @bodies.first.pos)
+    end
+    draw_circle(bc_sc.x, bc_sc.y, 20 * @scale * MPP, Gosu::Color::RED)
     @bodies.each do |body|
-      draw_circle(body.pos.x * @scale, body.pos.y * @scale, body.radius * @scale, Gosu::Color::WHITE)
+      screen_pos = world_to_screen(body.pos)
+      if screen_pos.x > @offset.x + 10000 || screen_pos.x < @offset.x - 10000 ||
+         screen_pos.y > @offset.y + 10000 || screen_pos.x < @offset.y - 10000
+        @bodies.delete(body)
+        puts "REMOVE: vel: #{"%.5e" % body.vel.x},#{"%.5e" % body.vel.y} pos: #{"%.5e" % body.pos.x},#{"%.5e" % body.pos.y}"
+        next
+      end
+      draw_circle(screen_pos.x, screen_pos.y, body.radius * @scale / MPP, Gosu::Color::WHITE)
     end
     if @display
       @font.draw_text("N: #{@bodies.size}", 600, 20, 1)
       @font.draw_text("T: #{@spt}", 600, 45, 1)
-      @font.draw_text("S: #{@scale.round(1)}", 600, 70, 1)
+      @font.draw_text("S: #{@scale.round(5)}", 600, 70, 1)
       @font.draw_text("E: #{elapsed}", 600, 95, 1)
     end
   end
